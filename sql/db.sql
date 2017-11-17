3d4c0561723dae572627798d5ddc716d9bd11af6
liszek <mateusz.lichon@gmail.com>
2017-11-17 15:30:54 +0100

    Add User class

--- sql/db.sql
+++ sql/db.sql
@@ -1,1 +1,13 @@
-CREATE TABLE user_group( id INT(11), name VARCHAR(255), PRIMARY KEY(id));
+CREATE TABLE user_group(
+id INT(11) AUTO_INCREMENT,
+name VARCHAR(255),
+PRIMARY KEY(id));
+
+CREATE TABLE users(
+id BIGINT(20) AUTO_INCREMENT,
+username VARCHAR(255),
+email VARCHAR(255) UNIQUE,
+password VARCHAR(245),
+user_group_id INT(11),
+PRIMARY KEY(id),
+FOREIGN KEY(user_group_id) REFERENCES user_group(id));
